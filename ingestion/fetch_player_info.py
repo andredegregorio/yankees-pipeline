a165cdae1e0982415c2a6605308dafa208528f5b
Andre DeGregorio <andrejdeg@gmail.com>
2026-05-22 11:56:13 -0400

    feat: add player info ingestion script

---
+++ ingestion/fetch_player_info.py
@@ -0,0 +1,30 @@
+from fetch_roster import fetch_roster_data, upload_to_s3
+import requests
+from datetime import datetime
+
+BASE_URL = "https://statsapi.mlb.com/api/v1"
+
+def fetch_player_info(team_id):
+	roster = fetch_roster_data(team_id)
+	if not roster:
+		print("Failed to fetch roster data.")
+		return
+	
+	today = datetime.today().strftime('%Y-%m-%d')
+
+	for player in roster.get("roster", []):
+		player_id = player.get("person", {}).get("id")
+		if not player_id:
+			continue
+			
+		try:
+			response = requests.get(f"{BASE_URL}/people/{player_id}")
+			response.raise_for_status()
+			data = response.json()
+			path = f"raw/player_info/{today}/player_{player_id}.json"
+			upload_to_s3(data, path)
+		except Exception as e:
+			print(f"Error fetching player info for player {player_id}: {e}")
+
+if __name__ == "__main__":
+	fetch_player_info(147)  # Fetch player info for the New York Yankees (team ID 147)
